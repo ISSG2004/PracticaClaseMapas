3d25e15f037e1e21fb00fa59585edb9e67022f74
ISSG2004 <ismaelsangon1234@gmail.com>
2024-11-28 13:42:10 +0100

    Tocamos el gitIgnore para la API key y añadimos las primeras lógicas para añadir un mapa con su fragment

---
+++ app/src/main/java/com/example/mapas281124/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.mapas281124
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mapa:GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        iniciarFragment()
+    }
+
+    private fun iniciarFragment() {
+        val fragment= SupportMapFragment()
+        fragment.getMapAsync(this)
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(R.id.fm_mapa,fragment)
+        }
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mapa=p0
+    }
+}
